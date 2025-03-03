301f1a9cb11e54f3c5fc69aba3ca02089e7e2bf8
유윤지 <105447233+llcodingll@users.noreply.github.com>
2025-03-03 14:07:41 +0900

    [D1] Title: 우주 괴물, Time: 83 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D1/23795. 우주 괴물/우주 괴물.java
+++ SWEA/D1/23795. 우주 괴물/우주 괴물.java
@@ -1,20 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    //괴물의 광선 상, 하, 좌, 우 델타값
+    static int[] monsterLaserI = {-1, 1, 0, 0};
+    static int[] monsterLaserJ = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
         for (int t = 1; t <= T; t++) {
-            int N = sc.nextInt();
+            int N = Integer.parseInt(br.readLine());
 
             int[][] area = new int[N][N];
             //괴물의 위치가 2로 표시되므로 for문을 돌면서 2를 찾으면 현위치로 저장
             int monsterCurrI = 0;
             int monsterCurrJ = 0;
             for (int i = 0; i < N; i++) {
+                String[] inputs = br.readLine().split(" ");
                 for (int j = 0; j < N; j++) {
-                    area[i][j] = sc.nextInt(); //입력
+                    area[i][j] = Integer.parseInt(inputs[j]); //입력
                     if (area[i][j] == 2) {
                         monsterCurrI = i; //2
                         monsterCurrJ = j; //4
@@ -22,14 +30,9 @@ public class Solution {
                 }
             } //입력 + 괴물 위치 저장
 
-            //괴물의 광선 상, 하, 좌, 우 델타값
-            int[] monsterLaserI = {-1, 1, 0, 0};
-            int[] monsterLaserJ = {0, 0, -1, 1};
-
             //괴물의 광선 나가는 방향
             int nextLaserI;
             int nextLaserJ;
-
 
             for (int d = 0; d < 4; d++) {
                 for (int k = 1; k < N; k++) {
