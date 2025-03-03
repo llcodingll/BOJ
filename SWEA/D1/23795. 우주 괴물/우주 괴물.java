4a1480cfc1ff062a1496a55852d8f6ff4ebecec5
유윤지 <105447233+llcodingll@users.noreply.github.com>
2025-03-03 14:12:59 +0900

    Update 우주 괴물.java

--- SWEA/D1/23795. 우주 괴물/우주 괴물.java
+++ SWEA/D1/23795. 우주 괴물/우주 괴물.java
@@ -2,9 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution {
+public class SpaceMonster {
 
-    //괴물의 광선 상, 하, 좌, 우 델타값
+    // 괴물의 광선 상, 하, 좌, 우 델타값
     static int[] monsterLaserI = {-1, 1, 0, 0};
     static int[] monsterLaserJ = {0, 0, -1, 1};
 
@@ -15,49 +15,66 @@ public class Solution {
         for (int t = 1; t <= T; t++) {
             int N = Integer.parseInt(br.readLine());
 
+            // 입력 및 괴물 위치 저장
             int[][] area = new int[N][N];
-            //괴물의 위치가 2로 표시되므로 for문을 돌면서 2를 찾으면 현위치로 저장
-            int monsterCurrI = 0;
-            int monsterCurrJ = 0;
-            for (int i = 0; i < N; i++) {
-                String[] inputs = br.readLine().split(" ");
-                for (int j = 0; j < N; j++) {
-                    area[i][j] = Integer.parseInt(inputs[j]); //입력
-                    if (area[i][j] == 2) {
-                        monsterCurrI = i; //2
-                        monsterCurrJ = j; //4
-                    }
-                }
-            } //입력 + 괴물 위치 저장
+            int[] monsterPosition = new int[2];
+            area = readArea(br, N, monsterPosition);
 
-            //괴물의 광선 나가는 방향
-            int nextLaserI;
-            int nextLaserJ;
+            // 괴물의 광선 발사 적용
+            applyMonsterLaser(area, N, monsterPosition);
 
-            for (int d = 0; d < 4; d++) {
-                for (int k = 1; k < N; k++) {
-                    nextLaserI = monsterCurrI + monsterLaserI[d]*k;
-                    nextLaserJ = monsterCurrJ + monsterLaserJ[d]*k;
-                    if (nextLaserI < N && nextLaserI >= 0 && nextLaserJ >= 0 && nextLaserJ < N) {
-                        if(area[nextLaserI][nextLaserJ] == 0) {
-                            area[nextLaserI][nextLaserJ] = 1;
-                        } else if(area[nextLaserI][nextLaserJ] == 1){
-                            break;
-                        }
-                    }
-                }
-            }
-
-            //0인 부분만 카운트
-            int cnt = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if(area[i][j] == 0){
-                        cnt++;
-                    }
-                }
-            }
-            System.out.println("#"+t+" "+cnt);
+            // 0인 부분 카운트
+            int cnt = countZeros(area, N);
+            System.out.println("#" + t + " " + cnt);
         }
+    }
+
+    private static int[][] readArea(BufferedReader br, int N, int[] monsterPosition) throws IOException {
+        int[][] area = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String[] inputs = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                area[i][j] = Integer.parseInt(inputs[j]);
+                if (area[i][j] == 2) {
+                    monsterPosition[0] = i;
+                    monsterPosition[1] = j;
+                }
+            }
+        }
+        return area;
+    }
+
+    private static void applyMonsterLaser(int[][] area, int N, int[] monsterPosition) {
+        int monsterCurrI = monsterPosition[0];
+        int monsterCurrJ = monsterPosition[1];
+
+        int nextLaserI;
+        int nextLaserJ;
+
+        for (int d = 0; d < 4; d++) {
+            for (int k = 1; k < N; k++) {
+                nextLaserI = monsterCurrI + monsterLaserI[d] * k;
+                nextLaserJ = monsterCurrJ + monsterLaserJ[d] * k;
+                if (nextLaserI < N && nextLaserI >= 0 && nextLaserJ >= 0 && nextLaserJ < N) {
+                    if (area[nextLaserI][nextLaserJ] == 0) {
+                        area[nextLaserI][nextLaserJ] = 1;
+                    } else if (area[nextLaserI][nextLaserJ] == 1) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private static int countZeros(int[][] area, int N) {
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (area[i][j] == 0) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 }
