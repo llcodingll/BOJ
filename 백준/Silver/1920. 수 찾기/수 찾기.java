28c94751ae501a6f7096cd7409112537fd2593ce
유윤지 <105447233+llcodingll@users.noreply.github.com>
2025-03-07 20:54:38 +0900

    Update 수 찾기.java

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,38 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Main {
+public class BOJ_SortNums {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Set<Integer> A = new HashSet<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> nums = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            A.add(Integer.parseInt(st.nextToken()));
+            nums.add(Integer.parseInt(br.readLine()));
         }
 
-        int M = Integer.parseInt(br.readLine());
-        int[] nums = new int[M];
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            nums[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if(A.contains(nums[i])){
-                    System.out.println(1);
-                    break;
-                } else {
-                    System.out.println(0);
-                    break;
-                }
-            }
+        Collections.sort(nums);
+        for(int result : nums){
+            System.out.println(result);
         }
     }
 }
