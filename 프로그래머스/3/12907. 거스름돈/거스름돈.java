51ed84e31f7d1c3e1c7bfcb03ba697d2141da328
유윤지 <105447233+llcodingll@users.noreply.github.com>
2025-04-06 19:35:36 +0900

    [level 3] Title: 거스름돈, Time: 10.06 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int[] dp;
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        dp = new int[n+1];
+		dp[0] = 1;
+		for(int coin : money) {
+			for (int i = coin; i <= n; i++) {
+				dp[i]+=dp[i-coin];
+			}
+            answer = dp[n];
+		}
+        return answer;
+    }
+}
