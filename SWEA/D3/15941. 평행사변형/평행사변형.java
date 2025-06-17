daf06d74f78335dfa1564f00da331f0576b0bed7
유윤지 <105447233+llcodingll@users.noreply.github.com>
2025-06-17 23:02:20 +0900

    [D3] Title: 평행사변형, Time: 73 ms, Memory: 24,576 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int area = N * N;
+            sb.append("#").append(t).append(" ").append(area).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
