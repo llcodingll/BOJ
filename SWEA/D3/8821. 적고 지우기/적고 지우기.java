cbebc054586fcc8f65ec3196834016efdc1e79f3
유윤지 <105447233+llcodingll@users.noreply.github.com>
2025-07-22 23:05:38 +0900

    [D3] Title: 적고 지우기, Time: 172 ms, Memory: 45,464 KB -BaekjoonHub

---
+++ SWEA/D3/8821. 적고 지우기/적고 지우기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int t = 1; t <= T; t++) {
+            String line = br.readLine();
+            HashSet<Character> notebook = new HashSet<>();
+
+            for (char ch : line.toCharArray()) {
+                if (notebook.contains(ch)) {
+                    notebook.remove(ch);
+                } else {
+                    notebook.add(ch);
+                }
+            }
+
+            sb.append("#").append(t).append(" ").append(notebook.size()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
