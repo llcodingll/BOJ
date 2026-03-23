88384e463477e2b23e72eab633dc686de622b5b8
유윤지 <105447233+llcodingll@users.noreply.github.com>
2026-03-23 21:28:53 +0900

    [Bronze III] Title: 홀수, Time: 124 ms, Memory: 15952 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.java
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -6,21 +6,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[7];
-        for (int i = 0; i < 7; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
         int num = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < 7; i++) {
-            if (arr[i] % 2 == 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+
+            if (a % 2 == 0) {
                 continue;
             }
-            num += arr[i];
-            if (arr[i] < min) {
-                min = arr[i];
+            num += a;
+            if (a < min) {
+                min = a;
             }
         }
 
